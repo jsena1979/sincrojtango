4d48c3bfffcc824664c49630c7f5dd1c85ee36a1
Jose Sena <senajosee@gmail.com>
2020-08-10 09:58:32 -0300

    Nuevo metodo en controlador
    Se agrega en e lemetodo boot un largo de default para mysql 5

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,18 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App;
 class PagesController extends Controller
 {
     //
     public function inicio(){
-        return view('welcome');
+        $consultasApi = App\ult_consulta_api::all();
+        return view('welcome',compact('consultasApi'));
+    }
+    public function detalle($id){
+        $consultas = App\ult_consulta_api::findOrFail($id);
+        return view('consultasApi.detalle',compact('consultas'));
     }
     public function panel(){
         return view('panel');
     }
 
-    public function procesos(){
+    public function procesos($nombre = null){
         $proceso = ['Sincronizar Stock', 'Sincronizar Precios', 'Sincronizar Articulos'];
         //return view('procesos',['proceso'=>$procesos,'nombre'=>$nombre]);
         return view('procesos', compact('proceso','nombre'));
